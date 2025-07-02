4261de789b61347bafe43d95028a6230c962160a
Paul-Elliot <peada@free.fr>
2025-07-02 12:41:19 +0200

    Allow `(documentation (url))` in toplevel dune-project
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/dune_lang/package.ml
+++ src/dune_lang/package.ml
@@ -20,7 +20,6 @@ type t =
   ; synopsis : string option
   ; description : string option
   ; depends : Package_dependency.t list
-  ; documentation : Package_documentation.t
   ; conflicts : Package_dependency.t list
   ; depopts : Package_dependency.t list
   ; info : Package_info.t
@@ -45,7 +44,6 @@ let deprecated_package_names t = t.deprecated_package_names
 let set_has_opam_file t has_opam_file = { t with has_opam_file }
 let version t = t.version
 let depends t = t.depends
-let documentation t = t.documentation
 let conflicts t = t.conflicts
 let depopts t = t.depopts
 let tags t = t.tags
@@ -66,7 +64,6 @@ let encode
       ; synopsis
       ; description
       ; depends
-      ; documentation
       ; conflicts
       ; depopts
       ; info
@@ -82,16 +79,12 @@ let encode
   =
   let open Encoder in
   let fields =
-    Package_info.encode_fields ~include_documentation:false info
+    Package_info.encode_fields info
     @ record_fields
         [ field "name" Name.encode name
         ; field_o "synopsis" string synopsis
         ; field_o "description" string description
         ; field_l "depends" Package_dependency.encode depends
-        ; field_i
-            "documentation"
-            (Package_documentation.encode ~url:(Package_info.documentation info))
-            documentation
         ; field_l "conflicts" Package_dependency.encode conflicts
         ; field_l "depopts" Package_dependency.encode depopts
         ; field_o "version" Package_version.encode version
@@ -138,14 +131,9 @@ let decode =
        and+ version =
          field_o "version" (Syntax.since Stanza.syntax (2, 5) >>> Package_version.decode)
        and+ depends = field ~default:[] "depends" (repeat Package_dependency.decode)
-       and+ url, documentation =
-         field
-           ~default:(None, { Package_documentation.packages = [] })
-           "documentation"
-           Package_documentation.decode
        and+ conflicts = field ~default:[] "conflicts" (repeat Package_dependency.decode)
        and+ depopts = field ~default:[] "depopts" (repeat Package_dependency.decode)
-       and+ info = Package_info.decode ~include_documentation:false ~since:(2, 0) ()
+       and+ info = Package_info.decode ~toplevel:false ~since:(2, 0) ()
        and+ tags = field "tags" (enter (repeat string)) ~default:[]
        and+ exclusive_dir =
          field_o
@@ -172,7 +160,6 @@ let decode =
            "Site location name"
        and+ allow_empty = field_b "allow_empty" ~check:(Syntax.since Stanza.syntax (3, 0))
        and+ lang_version = Syntax.get_exn Stanza.syntax in
-       let info = Package_info.set_documentation_url info url in
        let allow_empty = lang_version < (3, 0) || allow_empty in
        let id = Id.create ~name ~dir in
        let opam_file = Name.file id.name ~dir:id.dir in
@@ -181,7 +168,6 @@ let decode =
        ; synopsis
        ; description
        ; depends
-       ; documentation
        ; conflicts
        ; depopts
        ; info
@@ -210,7 +196,6 @@ let to_dyn
       ; synopsis
       ; description
       ; depends
-      ; documentation
       ; conflicts
       ; depopts
       ; info
@@ -231,7 +216,6 @@ let to_dyn
     ; "synopsis", option string synopsis
     ; "description", option string description
     ; "depends", list Package_dependency.to_dyn depends
-    ; "documentation", Package_documentation.to_dyn documentation
     ; "conflicts", list Package_dependency.to_dyn conflicts
     ; "depopts", list Package_dependency.to_dyn depopts
     ; "info", Package_info.to_dyn info
@@ -256,7 +240,6 @@ let create
       ~version
       ~conflicts
       ~depends
-      ~documentation
       ~depopts
       ~info
       ~has_opam_file
@@ -277,7 +260,6 @@ let create
   ; synopsis
   ; description
   ; depends
-  ; documentation
   ; conflicts
   ; info
   ; depopts
