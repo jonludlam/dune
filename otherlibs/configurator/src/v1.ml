159baa401f3934eeeee0e219e8d314215467f1e3
Jon Ludlam's Agent <jon+agent@recoil.org>
2026-02-17 13:35:46 +0000

    fix: resolve oxcaml locality errors in configurator

--- otherlibs/configurator/src/v1.ml
+++ otherlibs/configurator/src/v1.ml
@@ -134,13 +134,13 @@ module Process = struct
     let prog_command_line = command_line prog args in
     logf t "run: %s" prog_command_line;
     let n = gen_id t in
-    let create_process =
+    let create_process stdin stdout stderr =
       let args = Array.of_list (prog :: args) in
       match env with
-      | None -> Unix.create_process prog args
+      | None -> Unix.create_process prog args stdin stdout stderr
       | Some env ->
         let env = Array.of_list env in
-        Unix.create_process_env prog args env
+        Unix.create_process_env prog args env stdin stdout stderr
     in
     let stdout_fn = t.dest_dir ^/ sprintf "stdout-%d" n in
     let stderr_fn = t.dest_dir ^/ sprintf "stderr-%d" n in
@@ -790,12 +790,12 @@ let main ?(args = []) ~name f =
   let usage = sprintf "%s [OPTIONS]" (Filename.basename Sys.executable_name) in
   Arg.parse args anon usage;
   let log_db = ref [] in
-  let log s = log_db := s :: !log_db in
+  let log s = log_db := (s : string) :: !log_db in
   try
     let t =
       create_from_inside_dune
         ~dest_dir:!dest_dir
-        ~log:(if !verbose then prerr_endline else log)
+        ~log:(fun s -> if !verbose then prerr_endline s else log s)
         ~build_dir
         ~name
     in
